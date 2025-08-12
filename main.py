e5188501b232135213a50188d5a30a1c34843207
Jack Harper <jack.harper@stfc.ac.uk>
2025-08-12 13:21:30 +0100

    push messages - no nexus_structure yet

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 import asyncio
 
 
-from aioca import camonitor
+from aioca import caget, camonitor
 from aiokafka import AIOKafkaProducer
 from epicscorelibs.ca.dbr import DBR_CHAR_BYTES
 from ibex_non_ca_helpers.hex import dehex_decompress_and_dejson
@@ -12,7 +12,7 @@ from ibex_non_ca_helpers.hex import dehex_decompress_and_dejson
 from streaming_data_types.run_start_pl72 import serialise_pl72
 from streaming_data_types.run_stop_6s4t import serialise_6s4t
 
-logger = logging.getLogger("blah123")
+logger = logging.getLogger("borzoi")
 logging.basicConfig(level=logging.INFO)
 
 
@@ -49,24 +49,42 @@ class RunStarter:
 
         if value == "BEGINNING":
             self.current_job_id = str(uuid.uuid4())
-            await self.construct_and_send_runstart(self.current_job_id, self.prefix)
+            await self.construct_and_send_runstart(self.current_job_id)
         elif value == "ENDING":
             await self.construct_and_send_runstop(self.current_job_id)
 
+    async def construct_and_send_runstart(self, job_id: str):
+        logger.info(f"Sending run start with job_id: {job_id}")
+        start_time_s = await caget(f"{self.prefix}DAE:START_TIME")
+        start_time_ms = int(start_time_s * 1000)
+        logger.info(f"Start time: {start_time_ms}")
+
+        runnum = await caget(f"{self.prefix}DAE:RUNNUMBER")
+
+        # TODO construct filename here - wants to be hostname minus NDX prefix(?) + rbnum .nxs
+        filename = f"TEST_{runnum}.nxs"
+
+        blob = serialise_pl72(
+            job_id,
+            filename=filename,
+            start_time=start_time_ms,
+            nexus_structure="{}",
+            instrument_name="TEST",
+        )
+        await self.producer.send(self.topic, blob)
+
     async def construct_and_send_runstop(self, job_id: str):
         logger.info(f"Sending run stop with job_id: {job_id}")
-        # TODO get run stop time here
-        # serialise_6s4t(self.current_job_id)
-        await self.producer.send(self.topic, f"run stop with job_id: {job_id}".encode())
+        # stop_time only gets set to a non-zero value when the runstate goes back to SETUP.
+        # This is dirty, but poll it every 0.5 seconds until it does.
+        while await caget(f"{self.prefix}DAE:RUNSTATE", datatype=str) != "SETUP":
+            await asyncio.sleep(0.5)
 
-    async def construct_and_send_runstart(self, job_id: str, prefix):
-        logger.info(f"Sending run start with job_id: {job_id}")
-        # TODO get start time as unix timestamp here
-        # TODO construct nexus json here
-        # serialise_pl72()
-        await self.producer.send(
-            self.topic, f"run start with job_id: {job_id}".encode()
-        )
+        stop_time_s = await caget(f"{self.prefix}DAE:STOP_TIME")
+        stop_time_ms = int(stop_time_s * 1000)
+        logger.info(f"stop time: {stop_time_ms}")
+        blob = serialise_6s4t(job_id, stop_time=stop_time_ms)
+        await self.producer.send(self.topic, blob)
 
 
 async def set_up_producer(broker: str):
@@ -78,7 +96,7 @@ async def set_up_producer(broker: str):
 def main():
     prefix = os.environ.get("MYPVPREFIX")
 
-    broker = "localhost:19092"
+    broker = "livedata.isis.cclrc.ac.uk:31092"
     topic = "jacktestconfig"
 
     loop = asyncio.new_event_loop()
